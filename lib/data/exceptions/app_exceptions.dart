4ff5718f3abd110df8747c30f2c5365f40c1a798
samabir <abirchanda12@gmail.com>
2023-12-14 23:05:32 +0530

    commit

---
+++ lib/data/exceptions/app_exceptions.dart
@@ -0,0 +1,23 @@
+class AppExceptions{
+  final _message;
+  final _prefix;
+
+  AppExceptions(this._message, this._prefix);
+
+  @override
+  String toString() {
+    return '$_prefix$_message';
+  }
+}
+
+class InternetException extends AppExceptions{
+  InternetException([String? message]): super(message,"No Internet");
+}
+
+class RequestTimeOut extends AppExceptions{
+  RequestTimeOut([String? message]): super(message,"Request Time Out");
+}
+
+class ServerException extends AppExceptions{
+  ServerException([String? message]): super(message,"Internal server error");
+}
---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,16 @@
+import 'package:tender_app/data/network/base_api_services.dart';
+
+class NetworkApiServices extends BaseApiServices{
+  @override
+  Future getApi(String url) {
+    // TODO: implement getApi
+    throw UnimplementedError();
+  }
+
+  @override
+  Future postApi(String url, body) {
+    // TODO: implement postApi
+    throw UnimplementedError();
+  }
+  
+}
